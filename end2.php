7fcd571cefa78382905b0ed8469519c3db210608
mmohh2 <moh19951@outlook.com>
2019-12-05 15:20:41 +0100

    Update end2.php

--- end2.php
+++ end2.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Opdracht 3.3</title>
 </head>
 <body>
-    <h1>Stijn Papier Schaar</h1>
+    <h1>Steen Papier Schaar</h1>
     <hr>
     <?php
         $computer = rand(1,3);
         if ($computer == 1) {
-            $pc2 = "Stijn";
+            $pc2 = "Steen";
         }
         if ($computer == 2) {
             $pc2 = "Papier";
@@ -24,8 +24,8 @@
         $speler2 = $pc2;
         echo "<h1>Speler 1 koos " . $speler1 . ".</h1>";
         echo "<h1>Speler 2 koos " . $speler2 . ".</h1>";
-        if ($speler1 == "Stijn") {
-            if ($speler2 == "Stijn") {
+        if ($speler1 == "Steen") {
+            if ($speler2 == "Steen") {
                 echo "Gelijkspel";
             }
             if ($speler2 == "Papier") {
@@ -36,7 +36,7 @@
             }
         }
         if ($speler1 == "Papier") {
-            if ($speler2 == "Stijn") {
+            if ($speler2 == "Steen") {
                 echo "Speler 1 wint";
             }
             if ($speler2 == "Papier") {
@@ -47,7 +47,7 @@
             }
         }
         if ($speler1 == "Schaar") {
-            if ($speler2 == "Stijn") {
+            if ($speler2 == "Steen") {
                 echo "Speler 2 wint";
             }
             if ($speler2 == "Papier") {
