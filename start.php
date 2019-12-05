214b3fb07966bd51d62ab12295bcdbde0bd156d8
mmohh2 <moh19951@outlook.com>
2019-12-05 15:21:52 +0100

    Update start.php

--- start.php
+++ start.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Stijn, Papier, Schaar</title>
+	<title>Steen, Papier, Schaar</title>
 </head>
 <body>
-		<h1>Stijn Papier Schaar</h1>
+		<h1>Steen Papier Schaar</h1>
 		<h2>Wil je tegen computer of speler spelen?</h2>
         <a href="game.php">tegen speler</a>
         <a href="computer.php">tegen computer</a>
