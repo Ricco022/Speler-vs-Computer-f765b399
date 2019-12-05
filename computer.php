611ee0fa7b66ba76b7c241917b754d5a5a158d9a
mmohh2 <moh19951@outlook.com>
2019-12-05 15:15:42 +0100

    Update computer.php

--- computer.php
+++ computer.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Stijn, Papier, Schaar</title>
+	<title>Steen, Papier, Schaar</title>
 </head>
 <body>
-        <h1>Stijn Papier Schaar</h1>
+        <h1>Steen Papier Schaar</h1>
 		<h2>Speler 1</h2>
 		<?php
 		$dis = "";
 		if(isset($_GET["submit"]))
 			$dis = "display: none;";
 		echo "<form method='get' action='end2.php' style='" . $dis
-		. "'><select name='drop'><option>Stijn</option><option>Papier</option><option>Schaar</option></select><input type='submit' value='submit' name='submit'></form>";
+		. "'><select name='drop'><option>Steen</option><option>Papier</option><option>Schaar</option></select><input type='submit' value='submit' name='submit'></form>";
 			if (isset($_GET["submit"])) {
 				$p1c = $_GET["drop"];
 			}
